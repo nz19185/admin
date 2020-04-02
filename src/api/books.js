eef42042d5084324e371aac5b58e62c2c7f53b20
wu <123456@qq.com>
2020-04-02 16:08:22 +0800

    '增加按类查询'

--- src/api/books.js
+++ src/api/books.js
@@ -9,27 +9,38 @@ class Books {
 //     let url ='/hehe/book/info'
 //     return axios.post(url)
 //   }
+//分页查询
   list(page=1,pageSize=2){
     // localhost:3000/goods?page=1&pageSize=5
     let url ='/hehe/book/getinfo'
     return axios.post(url,{page,pageSize})
   }
+  //删除书籍
   del(_id){
     let url ='/hehe/book/del'
     return axios.post(url,_id)
   }
+  //修改书籍
   update(_id){
     let url =`/hehe/book/update${_id}`
     return axios.post(url)
   }
+  //增加书籍
   add(name,auctor,price,booktype,desc,path){
     let url ='/hehe/book/add'
     // console.log(name,auctor,price,booktype,desc,path)
     return axios.post(url,{name,auctor,price,booktype,desc,path})
   }
+  //根据关键字查询
   findBookByKey(key){
     let url ='/hehe/book/getinfoKey'
     return axios.post(url,{key})
+  }
+  //根据类别查询
+  findBookByType(booktype){
+    let url='/hehe/book/getinfoByType'
+    return axios.post(url,{booktype})
+
   }
 }
 
--- src/pages/customNav/head.js
+++ src/pages/customNav/head.js
@@ -38,6 +38,24 @@ class head extends Component {
     render() {
         return (
             <div>
+                {/* 根据书籍类别查询 */}
+                 <span style={{marginRight:'20px'}}>
+                    <Search
+                    placeholder="请输入您喜欢的书籍类别"
+                    onSearch={async (value)=>{
+                          let result =await api.findBookByType(value)
+                          console.log(result)
+                          const pay =result.list
+                          if(result.list!=''){
+                            message.success('查询成功')
+                            this.props.history.push({pathname:'/admin/searchbook',state:pay})
+                          }else{
+                            message.error('查无此书')
+                          }       
+                    }}
+                    style={{ width: 200 }}
+                /></span>
+                {/* 根据书籍关键字查询 */}
                 <span style={{marginRight:'20px'}}>
                     <Search
                     placeholder="请输入您要查询的书籍"
